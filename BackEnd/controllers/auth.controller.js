fa8102ba019ca0284c4a5b3f5a33ff47bf893ccd
Ibtisam-txd <muhammad.ibtisam@txend.com>
2024-12-25 13:21:45 +0500

    Update auth.controller.js

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -5,18 +5,10 @@ require("dotenv").config();
 const EMAIL = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
 
-/**
- * If the email and password are correct, then return a token.
- * @param req - {
- * @param res - {
- * @returns a function.
- */
 const login = (req, res) => {
-  /* Destructuring the email and password from the request body. */
   const { email, password } = req.body;
 
   if (email === EMAIL && password === PASSWORD) {
-    /* Creating a token. */
     const token = jwt.sign({ email }, process.env.TOKEN_KEY, {
       expiresIn: "2h",
     });
