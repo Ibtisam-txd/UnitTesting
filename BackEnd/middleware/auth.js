eaf04bfab22a456674d3c7746698329b8a6f9f56
Ibtisam-txd <muhammad.ibtisam@txend.com>
2024-12-25 13:22:24 +0500

    Update auth.js

--- BackEnd/middleware/auth.js
+++ BackEnd/middleware/auth.js
@@ -2,14 +2,6 @@ const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
-/**
- * It checks if the token is valid and if it is, it decodes it and attaches the decoded token to the
- * request object
- * @param req - The request object.
- * @param res - The response object.
- * @param next - This is a function that is called when the middleware is complete.
- * @returns The next middleware function in the app.js file.
- */
 const verifyToken = (req, res, next) => {
   const token = String(req.headers.authorization)
     .replace(/^bearer|^jwt/i, "")
@@ -22,7 +14,6 @@ const verifyToken = (req, res, next) => {
         msg: "A token is required for authentication",
       });
     }
-    /* Verifying the token. */
     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
     req.userData = decoded;
   } catch (err) {
